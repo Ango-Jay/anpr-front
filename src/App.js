65a65679140d83fafc1785ec67e56b042a243ec7
AngoJay <angojeff433@gmail.com>
2021-10-21 09:08:52 +0100

    Push to git

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import "./styles.css";
-
 export default function App() {
   return (
-    <div className="App">
+    <div className="App class-g">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
