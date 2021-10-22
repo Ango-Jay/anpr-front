9846ce5ba10bad93a603832668b7bc4fba76d804
AngoJay <angojeff433@gmail.com>
2021-10-22 02:46:27 +0100

    edit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ export default function App() {
   return (
     <div className="App class-g">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
     </div>
   );
 }
